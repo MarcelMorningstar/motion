d725b4e157eac38f32a17ffd45f55c3464ca9933
Roland <90729098+MarcelMorningstar@users.noreply.github.com>
2022-05-28 14:47:53 +0300

    Database

--- app/Http/Controllers/DealersController.php
+++ app/Http/Controllers/DealersController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Dealer;
+use App\Models\Country;
 
 class DealersController extends Controller
 {
     function show()
     {
-        return view('dealers');
+        return view('dealers') -> with(['dealers' => Dealer::all(), 'countries' => Country::all()]);
     }
 }
---
+++ database/migrations/2022_05_27_150443_create_dealers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dealers', function (Blueprint $table) {
+            $table -> id('ID') -> unsigned();
+            $table -> string('name', 32);
+            $table -> bigInteger('countryID') -> unsigned();
+            $table -> string('adress', 64);
+            $table -> string('phone', 20);
+            $table -> foreign('countryID') -> references('ID') -> on('countries');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('dealers');
+    }
+};
